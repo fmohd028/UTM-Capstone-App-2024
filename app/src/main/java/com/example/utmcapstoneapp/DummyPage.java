68683144830bed700af8a2aef296546f441837b3
fmohd028 <fmohd0849@gmail.com>
2024-01-08 13:00:37 +0800

    Made some updates. Remember, DummyPage is actually the reservation room selection page.

--- app/src/main/java/com/example/utmcapstoneapp/DummyPage.java
+++ app/src/main/java/com/example/utmcapstoneapp/DummyPage.java
@@ -1,15 +1,39 @@
 package com.example.utmcapstoneapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class DummyPage extends AppCompatActivity {
 //This is the code page for the room selection page
+    FirebaseAuth auth;
+    Button button;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dummy_page);
+
+        auth = FirebaseAuth.getInstance();
+        button = findViewById(R.id.logout);
+        user = auth.getCurrentUser();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
